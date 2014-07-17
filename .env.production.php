074ff3cbc8465e4bb8c280637014d820db375901
James Kontargyris <james@jameskontargyris.co.uk>
2014-07-17 14:49:17 +0100

    Renamed .env.php to correct file name

--- .env.production.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'DB_HOST' => 'db527679291.db.1and1.com',
-    'DB_NAME' => 'db527679291',
-    'DB_USERNAME' => 'dbo527679291',
-    'DB_PASSWORD' => 'fipraiwo',
-    'MAIL_DRIVER' => 'mail',
-];
