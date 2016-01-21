c6b774700333e498adf138668e6130279a852f70
James Kontargyris <james@jameskontargyris.co.uk>
2016-01-21 10:25:05 +0000

    Updated Spad and AD lists

--- app/database/seeds/AccountDirectorTableSeeder.php
+++ app/database/seeds/AccountDirectorTableSeeder.php
@@ -10,27 +10,30 @@ class AccountDirectorTableSeeder extends DatabaseSeeder
         DB::table('account_directors')->delete();
 
         $account_directors = [
-            'Ann Kelly'           => 'ann.kelly@fipra.com',
-            'David Lawsky'        => 'david.lawsky@fipra.com',
-            'Dirk Hudig'          => 'dirk.hudig@fipra.com',
-            'Jan Ahlskog'         => 'jan.ahlskog@fipra.com',
-            'John Gray'           => 'john.gray@fipra.com',
-            'Laura Batchelor'     => 'laura.batchelor@fipra.com',
+            'Ann Kelly' => 'ann.kelly@fipra.com',
+            'David Lawsky' => 'david.lawsky@fipra.com',
+            'Dirk Hudig' => 'dirk.hudig@fipra.com',
+            'Jan Ahlskog' => 'jan.ahlskog@fipra.com',
+            'John Gray' => 'john.gray@fipra.com',
+            'Laura Batchelor' => 'laura.batchelor@fipra.com',
             'Peter-Carlo Lehrell' => 'mark.fielding@fipra.com',
-            'Mark MacGann'        => 'mark.macgann@fipra.com',
-            'Paul Buffet'         => 'paul.buffet@fipra.com',
-            'Nathalie Hesketh'    => 'nathalie.hesketh@fipra.com',
-            'Phil Evans'          => 'phil.evans@fipra.com',
-            'Rory Chisholm'       => 'rory.chisholm@fipra.com',
-            'Ukko Metsola'        => 'ukko.metsola@fipra.com',
-            'Paul Buffet'         => 'paul.buffet@fipra.com',
-            'Hilary Hudson'       => 'hilary.hudson@fipra.com'
+            'Mark MacGann' => 'mark.macgann@fipra.com',
+            'Paul Buffet' => 'paul.buffet@fipra.com',
+            'Nathalie Hesketh' => 'nathalie.hesketh@fipra.com',
+            'Phil Evans' => 'phil.evans@fipra.com',
+            'Rory Chisholm' => 'rory.chisholm@fipra.com',
+            'Ukko Metsola' => 'ukko.metsola@fipra.com',
+            'Paul Buffet' => 'paul.buffet@fipra.com',
+            'Hilary Hudson' => 'hilary.hudson@fipra.com',
+            'Martina Bianchini' => 'martina.bianchini@fipra.com',
+            'Helene Lloyd' => 'helene.lloyd@fipra.com',
+            'Willem Vriesendorp' => 'willem.vriesendorp@fipra.com',
         ];
 
         foreach ($account_directors as $name => $email) {
             Account_director::create(
                 [
-                    'name'  => $name,
+                    'name' => $name,
                     'email' => $email
                 ]
             );
