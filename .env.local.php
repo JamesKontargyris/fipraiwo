b872487fdae5797d75bf6eca99671a8bdb6fb275
James Kontargyris <james@jameskontargyris.co.uk>
2016-05-05 19:36:15 +0000

    Production DB access fix

---
+++ .env.local.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+
+    'DB_HOST'     => 'localhost',
+    'DB_NAME'     => 'iwo',
+    'DB_USERNAME' => 'homestead',
+    'DB_PASSWORD' => 'secret',
+    'DEBUG'       => 'true'
+
+);
