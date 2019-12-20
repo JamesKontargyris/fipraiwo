1869f48190dfae7d9931f1018856b9e58a055d69
James Kontargyris <james@jameskontargyris.co.uk>
2019-12-20 13:16:05 +0000

    Bug fix: jQuery datepicker plugin now fixed on Spad form

--- app/views/forms/spad.blade.php
+++ app/views/forms/spad.blade.php
@@ -192,13 +192,13 @@
             <div class="formfield">
                 {{ Form::label('internal_work_order_start_date', 'IWO start date:', ['class' => 'required']) }} <a href="#" class="help">&nbsp;</a>
                 <div class="help-box">The Start date is from when payments can be made under this IWO.</div>
-                {{ Form::text('internal_work_order_start_date', (editing()) ? isset($workorder->workorder->internal_work_order_start_date) ? $workorder->workorder->internal_work_order_start_date : '' : Input::old('internal_work_order_start_date'), ['class' => 'datepicker']) }}
+                {{ Form::text('internal_work_order_start_date', (editing()) ? isset($workorder->workorder->internal_work_order_start_date) ? $workorder->workorder->internal_work_order_start_date : '' : Input::old('internal_work_order_start_date'), ['class' => 'datepicker', 'id' => 'internal_work_order_start_date_datepicker']) }}
                 {{ display_form_error('internal_work_order_start_date', $errors) }}
             </div>
             <div class="formfield">
                 {{ Form::label('internal_work_order_expiry_date', 'IWO expiry date:', ['class' => 'required']) }} <a href="#" class="help">&nbsp;</a>
                 <div class="help-box">This is the date payment will stop under this IWO.</div>
-                {{ Form::text('internal_work_order_expiry_date', (editing()) ? isset($workorder->workorder->internal_work_order_expiry_date) ? $workorder->workorder->internal_work_order_expiry_date : '' : Input::old('internal_work_order_expiry_date'), ['class' => 'datepicker']) }}
+                {{ Form::text('internal_work_order_expiry_date', (editing()) ? isset($workorder->workorder->internal_work_order_expiry_date) ? $workorder->workorder->internal_work_order_expiry_date : '' : Input::old('internal_work_order_expiry_date'), ['class' => 'datepicker', 'id' => 'internal_work_order_expiry_date_datepicker']) }}
                 {{ display_form_error('internal_work_order_expiry_date', $errors) }}
             </div>
 
