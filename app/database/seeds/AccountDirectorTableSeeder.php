80d4d0232e6349075be736b62b7c7b4b9a35163f
James Kontargyris <james@jameskontargyris.co.uk>
2020-01-23 14:21:37 +0000

    Removed Willem Vriesendorp; added Olivera Drazic to Account Directors seeder

--- app/database/seeds/AccountDirectorTableSeeder.php
+++ app/database/seeds/AccountDirectorTableSeeder.php
@@ -8,19 +8,19 @@ class AccountDirectorTableSeeder extends DatabaseSeeder {
 		DB::table( 'account_directors' )->delete();
 
 		$account_directors = [
-			'Daniel Furby'        => 'daniel.furby@fipra.com',
-			'David Lawsky'        => 'david.lawsky@fipra.com',
-			'Dirk Hudig'          => 'dirk.hudig@fipra.com',
-			'Hilary Hudson'       => 'hilary.hudson@fipra.com',
-			'Dayanthi Adeyemo'    => 'dayanthi.adeyemo@fipra.com',
-			'John Lion'           => 'john.lion@fipra.com',
-			'Laura Batchelor'     => 'laura.batchelor@fipra.com',
-			'Willem Vriesendorp'  => 'willem.vriesendorp@fipra.com',
-			'Rory Chisholm'       => 'rory.chisholm@fipra.com',
-			'Neil Causey'         => 'neil.causey@fipra.com',
-			'Rachel Finnegan'     => 'rachel.finnegan@fipra.com',
-			'Olga Bakardzhieva'   => 'olga.bakardzhieva@fipra.com',
-			'Robert Madelin'      => 'robert.madelin@fipra.com',
+			'Daniel Furby'      => 'daniel.furby@fipra.com',
+			'David Lawsky'      => 'david.lawsky@fipra.com',
+			'Dirk Hudig'        => 'dirk.hudig@fipra.com',
+			'Hilary Hudson'     => 'hilary.hudson@fipra.com',
+			'Dayanthi Adeyemo'  => 'dayanthi.adeyemo@fipra.com',
+			'John Lion'         => 'john.lion@fipra.com',
+			'Laura Batchelor'   => 'laura.batchelor@fipra.com',
+			'Olivera Drazic'    => 'olivera.drazic@fipra.com',
+			'Rory Chisholm'     => 'rory.chisholm@fipra.com',
+			'Neil Causey'       => 'neil.causey@fipra.com',
+			'Rachel Finnegan'   => 'rachel.finnegan@fipra.com',
+			'Olga Bakardzhieva' => 'olga.bakardzhieva@fipra.com',
+			'Robert Madelin'    => 'robert.madelin@fipra.com',
 		];
 
 		foreach ( $account_directors as $name => $email ) {
