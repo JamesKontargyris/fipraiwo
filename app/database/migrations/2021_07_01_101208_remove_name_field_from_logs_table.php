d5310d67773ab057e255ad1dc8f905fbf8612569
James Kontargyris <james@jameskontargyris.co.uk>
2021-07-01 10:14:26 +0000

    Added migration to remove name column from logs table

---
+++ app/database/migrations/2021_07_01_101208_remove_name_field_from_logs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RemoveNameFieldFromLogsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('logs', function($table)
+        {
+           $table->dropColumn('name');
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('logs', function($table)
+        {
+            $table->string('name')->after('type');
+        });
+	}
+
+}
